48e044424089e7b8ac0a2894634f3c732c3082bc
computer-mann <hpsn16@gmail.com>
2026-02-21 10:32:28 +0000

    replace all

---
+++ app-example/app/(tabs)/albums.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@react-navigation/elements";
+import { View } from "react-native";
+
+export default function AlbumsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Albums Screen</Text>
+    </View>
+  );
+}
---
+++ app-example/app/(tabs)/artists.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@react-navigation/elements";
+import { View } from "react-native";
+
+export default function ArtistsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Artists Screen</Text>
+    </View>
+  );
+}
